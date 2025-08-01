6d0887bfe4ae7a531f7075ba510083e404c51e1c
Andre Lellis <andrelellis@protonmail.com>
2025-08-01 12:32:32 +0100

    feat: implement exercises 1.7, 1.8 and 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,10 +6,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const total = good + neutral + bad;
-  const average = (good - bad) / total;
-  const positive = good / total;
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -17,14 +13,32 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = good / total;
+
+  return (
+    <>
+      <h2>statistics</h2>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {total}</p>
+          <p>average {average}</p>
+          <p>positive {positive * 100} %</p>
+        </>
+      )}
+    </>
   );
 };
 
