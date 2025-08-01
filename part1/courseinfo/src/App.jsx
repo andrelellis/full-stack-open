e05ab8de9017941587f3db08d2a7e5a8b2b6240a
Andre Lellis <andrelellis@protonmail.com>
2025-08-01 11:29:26 +0100

    feat: implement exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,24 +1,29 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
-  const exerciseTotal = part1.exercises + part2.exercises + part3.exercises;
+  const exerciseTotal = parts.reduce(
+    (numOfExercises, part) => numOfExercises + part.exercises,
+    0
+  );
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
+      <Content parts={parts} />
       <Total exerciseTotal={exerciseTotal} />
     </div>
   );
@@ -28,7 +33,6 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-// part and exercises arrays must have same length and be ordered
 const Content = ({ parts }) => {
   const content = parts.map(({ name, exercises }, index) => (
     <Part key={index} name={name} numOfExercises={exercises}></Part>
